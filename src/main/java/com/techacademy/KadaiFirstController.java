81c387b158902d7084747b0581c7d63c48d7d078
shingostar <shingostar@shore.ocn.ne.jp>
2024-01-17 23:38:07 +0900

    KadaiFirstSpringBootApplication.javaを提出の為。

--- src/main/java/com/techacademy/KadaiFirstController.java
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -1,8 +1,5 @@
 package com.techacademy;
 
-import java.time.LocalDate;
-import java.time.format.TextStyle;
-import java.util.Locale;
 import java.util.Calendar;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +7,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class KadaiFirstController<DayOfWeek> {
+public class KadaiFirstController {
 
-    @GetMapping("/dispDayOfWeek/{yyyymmdd}")
-    public String dispDayOfWeek(@PathVariable int yyyy,@PathVariable int mm,@PathVariable int dd) {
-        LocalDate ld = LocalDate.of(yyyy, mm, dd);
-        DayOfWeek w = ld.getDayOfWeek();
-        return w.getDisplayName(TextStyle.LocaleDate.JAPANESE);
-        }
+    @GetMapping("/dayofweek/{var1}") //曜日を取り出す
+    public String dispDayOfWeek(@PathVariable String var1) {
+
+        Integer i = Integer.valueOf(var1);
+        String str = i.toString();
+
+        int year = Integer.parseInt(str.substring(0, 4));   //最初の文字列の4文字まで切り出す
+        int month = Integer.parseInt(str.substring(4, 6));  //文字列の5〜6文字目まで切り出す
+        int date = Integer.parseInt(str.substring(6));  //文字列の7文字目まで切り出す
+
+        String[] week_name = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(year,month-1,date);
+        int dayOfWeek =cal.get(Calendar.DAY_OF_WEEK)-1; //曜日の値を0~6までの範囲で曜日を表すため1をマイナスします。
+
+        return week_name[dayOfWeek];
+    }
+
 
     @GetMapping("/plus/{val1}/{val2}")
     public String calcPlus(@PathVariable int val1, @PathVariable int val2) {

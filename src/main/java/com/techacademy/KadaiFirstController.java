e582304be5c0695c3cd7408b8849903ddaf84138
shingostar <shingostar@shore.ocn.ne.jp>
2024-03-02 01:10:12 +0900

    コミットします。

--- src/main/java/com/techacademy/KadaiFirstController.java
+++ src/main/java/com/techacademy/KadaiFirstController.java
@@ -1,6 +1,9 @@
 package com.techacademy;
 
-import java.util.Calendar;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,52 +11,36 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KadaiFirstController {
-
-    @GetMapping("/dayofweek/{var1}") //曜日を取り出す
-    public String dispDayOfWeek(@PathVariable String var1) {
-
-        Integer i = Integer.valueOf(var1);
-        String str = i.toString();
-
-        int year = Integer.parseInt(str.substring(0, 4));   //最初の文字列の4文字まで切り出す
-        int month = Integer.parseInt(str.substring(4, 6));  //文字列の5〜6文字目まで切り出す
-        int date = Integer.parseInt(str.substring(6));  //文字列の7文字目まで切り出す
-
-        String[] week_name = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(year,month-1,date);
-        int dayOfWeek =cal.get(Calendar.DAY_OF_WEEK)-1; //曜日の値を0~6までの範囲で曜日を表すため1をマイナスします。
-
-        return week_name[dayOfWeek];
+    @GetMapping("dayofweek/{date}")
+    public String dispDayOfWeek(@PathVariable String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate ld = LocalDate.parse(date,formatter);
+        DayOfWeek w = ld.getDayOfWeek();
+        return "実行結果：" + w ;
     }
 
-
-    @GetMapping("/plus/{val1}/{val2}")
-    public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
+    @GetMapping("plus/{val1}/{val2}")
+    public String calcPlus(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
         res = val1 + val2;
-        return "計算結果" + res;
-    }
-
-    @GetMapping("/minus/{val1}/{val2}")
-    public String calcMinus(@PathVariable int val1, @PathVariable int val2) {
+        return "実行結果：" + res;
+}
+    @GetMapping("minus/{val1}/{val2}")
+    public String calcMinus(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
         res = val1 - val2;
-        return "計算結果" + res;
-    }
-
-    @GetMapping("/times/{val1}/{val2}")
-    public String calcTimes(@PathVariable int val1, @PathVariable int val2) {
+        return "実行結果：" + res;
+}
+    @GetMapping("times/{val1}/{val2}")
+    public String calcTimes(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
         res = val1 * val2;
-        return "計算結果" + res;
-    }
-
-    @GetMapping("/divide/{val1}/{val2}")
-    public String calcDivide(@PathVariable int val1, @PathVariable int val2) {
+        return "実行結果：" + res;
+}
+    @GetMapping("divide/{val1}/{val2}")
+    public String calcDivide(@PathVariable int val1,@PathVariable int val2) {
         int res = 0;
         res = val1 / val2;
-        return "計算結果：" + res;
+        return "実行結果：" + res;
     }
 }
